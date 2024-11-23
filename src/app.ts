09bfc0b1c4fec144b3c3881f129b90c81d165866
luigiikk <luigimee15@gmail.com>
2024-11-23 01:35:29 -0300

    ✨feat: route register users

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,30 @@
 import fastify from "fastify";
-import {PrismaClient} from '@prisma/client'
+import {z} from "zod";
+import { prisma } from "./lib/prisma";
+import type { Prisma } from "@prisma/client";
+
+
 
 export const app = fastify();
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const {name, email, password} = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    } as Prisma.UserUncheckedCreateInput,
+  });
+
+  return reply.status(201).send();
+})
+
 
